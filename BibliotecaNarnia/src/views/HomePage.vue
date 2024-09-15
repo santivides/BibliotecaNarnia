ae5d3d27f18122700edf27e4dc0cf0a04786d3c0
santivides <santivides01@gmail.com>
2024-09-15 00:46:11 -0500

    pagina inicio creada pero falta mejorarla

--- BibliotecaNarnia/src/views/HomePage.vue
+++ BibliotecaNarnia/src/views/HomePage.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="home-container">
         <header>
-            <h1>Biblioteca Narnia</h1>
+            <div class="header-title">
+                <h1>Biblioteca Narnia</h1>
+            </div>
+            
+            <div class="header-button">
+                <RouterLink to="/login">
+                    <button>Iniciar sesión</button>
+                </RouterLink>
+                
+            </div>
         </header>
         <section class = 'page-body'>
             <h2>Expande tus horizontes a través de la lectura</h2>
             <p>Ven y conce nuestra colección de libros o mira aquí mismo</p>
-            <button>mirar catalogo</button>
+            <RouterLink to="/login">
+                <button>mirar catalogo</button>
+            </RouterLink>
+            
         </section>
     </div>
 </template>
@@ -22,9 +34,79 @@
 
 <style>
 
-.page-body{
-    background-color: #FFF8E8;
-    
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+.home-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh; /* Hace que el contenedor ocupe toda la altura de la ventana */
+}
+
+
+/* Estilo del header */
+header {
+  background-color: #674636;
+  height: 100px; /* Altura fija para el header */
+  padding: 10px;
+  display: flex;
+  justify-content: space-between; 
+  z-index: 2;
+}
+
+
+.header-title {
+    font: italic;
+    font-size: 24px;
+    font-weight: bold;
+    color: white;
+}
+
+.header-button{
+    align-self: center;   
+}
+
+.page-body {
+  background-color: #F7EED3;
+  display: flex;
+  flex-grow: 1; /* Hace que la sección principal ocupe todo el espacio restante */
+  flex-direction: column;
+  justify-content: center; /* Centra el contenido verticalmente */
+  align-items: flex-start; /* Alinea el contenido al inicio horizontalmente */
+  padding: 20px;
+  box-sizing: border-box;
+  color: black;
+  font-style: italic; 
+  z-index: 1;
+}
+
+.page-body h2 {
+  font-style: italic;
+  font-size: 40px;
+  font-weight: 600;
+  color: #674636;
+}
+
+.page-body p {
+  font-size: 16px;
+  color: #333333;
+}
+
+/* Estilo del botón */
+.page-body button, .header-button button{
+  background-color: #AAB396;
+  font-style: italic;
+  border-radius: 6px;
+  border: none;
+  padding: 10px 20px; /* Relleno adecuado para el botón */
+  margin-top: 10px; /* Margen superior para separar el botón del texto */
+  cursor: pointer;
+}
+
+.page-body button:hover, .header-button button:hover{
+  background-color: #8a9f78; /* Efecto hover en el botón */
 }
 
 </style>
---
+++ BibliotecaNarnia/src/views/LoginPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="login-container">
+        <h1>Inicion de sesión</h1>
+
+        <section class="formulary">
+            <p>Correo</p>
+            <input class="text" id="correo" name="correo">
+            
+            <p>Contraseña</p>
+            <input class="password" id="correo" name="correo">
+
+            <div class="login-buttons">
+                <button>iniciar sesión</button>
+                <button>registrarse</button>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default {
+    name: 'LogicComponent',
+    mounted() {
+        console.log('El componente se ha montado');
+    }
+    }
+</script>
+
+<style scoped>
+</style>
+
+
